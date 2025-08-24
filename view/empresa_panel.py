d5ec004422f821c9bc42eaeb8a2418d9a059e812
JuliaGattera <132839726+JuliaGattera@users.noreply.github.com>
2025-08-24 11:48:38 -0300

    Update empresa_panel.py

--- view/empresa_panel.py
+++ view/empresa_panel.py
@@ -1,6 +1,11 @@
-
+# view/empresa_panel.py
 import streamlit as st
 
-def show_empresa_panel():
-    st.title("Painel da Empresa")
-    st.write("Aqui você pode gerenciar suas vagas e candidaturas.")
+def show_empresa_panel(supabase, logout_func):
+    user = st.session_state.user
+    st.success(f"Login como Empresa realizado com sucesso! Bem-vindo, {user['nome']}")
+
+    if st.button("Logout"):
+        logout_func()
+
+    st.info("Funcionalidades da empresa ainda serão implementadas.")
