447cba6d87f764d36bb2e6605e41ae7912537c15
JuliaGattera <132839726+JuliaGattera@users.noreply.github.com>
2025-08-28 00:20:22 -0300

    Update empresa_panel.py

--- view/empresa_panel.py
+++ view/empresa_panel.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from view.empresa.criar_vaga import criar_vaga
-from view.empresa.listar_vagas import listar_vagas_com_candidatos  # Import atualizado
+from view.empresa.listar_vagas_com_candidatos import listar_vagas_com_candidatos  # Import atualizado
 
 def show_empresa_panel(supabase, logout_fn):
     user = st.session_state.user
