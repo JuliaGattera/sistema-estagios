8a739221330bdb38a2d63cebc334f5e68a0300b0
JuliaGattera <132839726+JuliaGattera@users.noreply.github.com>
2025-08-29 16:23:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.write("Streamlit versão:", st.__version__)
 from supabase_client import supabase
 from view.login import show_login_screen
 from view.admin_panel import show_admin_panel
